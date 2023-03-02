ff05a4bb50ba920e0cb4dd0212ca359dd6c4d080
alamincoder2001 <114378678+alamincoder2001@users.noreply.github.com>
2023-03-02 17:39:41 +0600

    Add all file

---
+++ application/config/database.php
@@ -0,0 +1,32 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+
+$db['default'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+
+	'username' => 'root',
+	'password' => '',
+	// 'username' => 'belcopha',
+	// 'password' => 'TN9wr2ryB;Z0+3',
+	'database' => 'belcopha_sstech',
+
+
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
